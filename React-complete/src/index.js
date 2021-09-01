2e19af038461cf4e75c12fc9e295fd0e2c8169b3
laisrmacedo <laisrodriguesmacedo@gmail.com>
2021-09-01 16:53:31 -0300

    Primeiro componente criado

--- React-complete/src/index.js
+++ React-complete/src/index.js
@@ -6,3 +6,4 @@ import App from './App';
 //Método Render - possui dois argumentos (o que sera renderizado, onde sera rederizado)
 // (componente, API JavaScript DOM)
 ReactDOM.render(<App />, document.getElementById('root'));
+// Componentes sao funçoes que retornam o codigo HTML
