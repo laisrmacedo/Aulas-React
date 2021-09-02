37713a10530cfd07f5aaa2af013f7867801c5be6
laisrmacedo <laisrodriguesmacedo@gmail.com>
2021-09-02 16:58:53 -0300

    Componente reutilizável

--- React-complete/src/components/ExpenseItem.js
+++ React-complete/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    const expenseDate = new Date(2021, 2, 9);
+    const expenseTitle = 'Car Insurence';
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>September 1th 2021</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurence</h2>
-                <div className="expense-item__price">$ 250</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
