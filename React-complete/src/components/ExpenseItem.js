b5aba2dea5ef1b9ecc9f2fbe647b440ebce99f5f
laisrmacedo <laisrodriguesmacedo@gmail.com>
2021-09-07 18:48:50 -0300

    Componente criado

--- React-complete/src/components/ExpenseItem.js
+++ React-complete/src/components/ExpenseItem.js
@@ -1,13 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
-    const expenseDate = new Date(2021, 2, 9);
-    const expenseTitle = 'Car Insurence';
-    const expenseAmount = 294.67;
-
+    
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <ExpenseDate date={props.date}/>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
